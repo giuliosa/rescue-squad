0040a4ed403f12f4b506ab6e0b75e2615f15812a
Giulio Sá <giuliocaetano@yahoo.com.br>
2022-06-27 10:13:47 -0300

    Initial Config

--- Scenes/Actors/Character.gd
+++ Scenes/Actors/Character.gd
@@ -24,9 +24,14 @@ func _process(delta):
 		
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("shoot_right") - Input.get_action_strength("shoot_left")
+	input_vector.y = Input.get_action_strength("shoot_down") - Input.get_action_strength("shoot_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
@@ -35,7 +40,6 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move()
-
 
 func move():
 	velocity = move_and_slide(velocity)
