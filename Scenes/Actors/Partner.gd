fc53b481764ffb1d36ae1e77f80c52126c7f3bc0
Giulio Sá <giuliocaetano@yahoo.com.br>
2022-08-24 14:39:43 -0300

    Initial pathfinding

--- Scenes/Actors/Partner.gd
+++ Scenes/Actors/Partner.gd
@@ -1,15 +1,59 @@
 extends KinematicBody2D
 
-export var ACCELERATION = 300
-export var MAX_SPEED = 50
-export var FRICTION = 200
-export var WANDER_TARGER_RANGE = 4
+export var ACCELERATION := 300
+export var MAX_SPEED := 50
+export var FRICTION := 200
+export var WANDER_TARGER_RANGE := 4
+
+
 
 enum {
 	IDLE,
-	WANDER,
-	CHASE
+	ALIGN,
+	CHASE,
+	ATTACK
 }
 
+var velocity = Vector2.ZERO
+var state := IDLE
+var player : KinematicBody2D = null
+var path : Array = []
+
+onready var raycast = $RayCast2D
+
 func _ready():
+	yield(get_tree(), "idle_frame")
+	
+	player = get_parent().get_node("Character")
+
+func _physics_process(delta: float) -> void:
+	if raycast.is_colliding():
+		print (raycast.get_collider())
+	state = ALIGN
+			
+	match state:
+		CHASE:
+			chase_state(delta)
+		ATTACK:
+			attack_state(delta)
+		ALIGN:
+			align_state(delta)
+		IDLE:
+			pass
+
+func chase_state(_delta):
 	pass
+	
+func attack_state(_delta):
+	pass
+	
+	
+func align_state(delta):
+	if player != null:
+		velocity = global_position.direction_to(player.global_position)
+		global_position += velocity * MAX_SPEED * delta
+		
+	velocity = move_and_slide(velocity)
+		
+
+
