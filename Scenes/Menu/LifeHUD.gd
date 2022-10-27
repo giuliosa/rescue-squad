d806eb0599710d870d6d07f3a85d5b8182f556c0
Giulio Sá <giuliocaetano@yahoo.com.br>
2022-10-27 14:47:46 -0300

    feat: HUD with stamina bar

--- Scenes/Menu/LifeHUD.gd
+++ Scenes/Menu/LifeHUD.gd
@@ -2,13 +2,13 @@ extends CanvasLayer
 
 
 func _ready():
-	PlayerStats.set_max_health_player($LifePlayer.rect_size.x)
-	PlayerStats.set_health_player($LifePlayer.rect_size.x)
+	PlayerStats.set_max_health_player($Player/Life.rect_size.x)
+	PlayerStats.set_health_player($Player/Life.rect_size.x)
 
-	PartnerStats.set_max_health_partner($LifeParter.rect_size.x)
-	PartnerStats.set_health_partner($LifeParter.rect_size.x)
+	PartnerStats.set_max_health_partner($Partner/Life.rect_size.x)
+	PartnerStats.set_health_partner($Partner/Life.rect_size.x)
 	
 func _process(_delta):
-	$LifePlayer.rect_size.x = PlayerStats.health_player
-	$LifeParter.rect_size.x = PartnerStats.health_partner
+	$Player/Life.rect_size.x = PlayerStats.health_player
+	$Partner/Life.rect_size.x = PartnerStats.health_partner
 	
