616c2de87646abc86c70ad3233285268a00e678e
Giulio Sá <giuliocaetano@yahoo.com.br>
2023-01-26 13:04:33 -0300

    Improving weapons chose

--- Scenes/Actors/Player/SpecialMovements.gd
+++ Scenes/Actors/Player/SpecialMovements.gd
@@ -11,26 +11,37 @@ onready var powerTimer := $PowerTimer
 
 func _ready():
 	AmmoStats.max_ammo = 6
-	powerTimer.start()	
+	gun.frame = 0
+	fire_rate = 0.3
+	powerTimer.start()
 
 
 func _process(delta):
 	gun.look_at(get_global_mouse_position())
 	
-	if Input.is_action_just_pressed("weapon_one"):
-		gun.frame = 6
-		fire_rate = 0.7
-	if Input.is_action_just_pressed("weapon_two"):
-		gun.frame = 7
-		fire_rate = 0.5
 	if Input.is_action_just_pressed("weapon_three"):
+		gun.frame = 10
+		fire_rate = 1.5
+	if Input.is_action_just_pressed("weapon_two"):
 		gun.frame = 8
+		fire_rate = 0.5
+	if Input.is_action_just_pressed("weapon_one"):
+		gun.frame = 0
 		fire_rate = 0.3
 
 
 func fire():
 	if(can_fire):
 		var bullet = bullet_scene.instance()
+		
+		match gun.frame:
+			10:
+				bullet.damage = 5
+			8: 
+				bullet.damage = 3
+			0:
+				bullet.damage = 2
+		
 		get_parent().add_child(bullet)
 		bullet.global_position = $Gun/Position.get_global_position()
 		var target = get_global_mouse_position()
